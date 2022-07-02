717bdaaf050dfb3954bc3ea28b0b9025efaae062
makingglitches <john.r.sohn@gmail.com>
2022-07-02 15:17:03 -0400

    Updated ImageLocation object

--- storemgr/imagelocations.js
+++ storemgr/imagelocations.js
@@ -15,7 +15,31 @@ const EStoreType =
 {
     Trusted : 1,
     ServerOnly: 2, 
-    Originals: 3
+    Originals: 3,
+    FromStoreType: function(storetype,itemlocation)
+    {
+
+        itemlocation.TrustedStore = EStoreType.Trusted == storetype;
+        itemlocation.ServerOnlyOrganizer = EStoreType.ServerOnly == storetype;
+        itemlocation.OriginalStore = EStoreType.Originals == storetype
+
+        return itemlocation;
+    },
+    ToStoreType: function (i)
+    {
+        if (i.TrustedStore)
+        {
+            return this.Trusted;
+        }
+        else if (i.ServerOnlyOrganizer)
+        {
+            return this.ServerOnly;
+        }
+        else if (i.OriginalStore)
+        {
+            return this.Originals;
+        }
+    }
 }
 
 class ImageLocation
@@ -35,16 +59,20 @@ class ImageLocation
             throw "Directory must exist at time of creation !\nEven if on a mounted device later umounted!\n"+directory;
         }
 
-        
-
         MountInfo.ParseMounts();
         
+        // get the MountInfo and relative path.
         var info = MountInfo.WhichDevice(directory);
 
         // autopopulate this, subtracts mountpoint
 
 
-        return new ImageLocation( info[0].UUID, info[0].SUBVOLUMEID,info[1],storetype,main,userid);
+        return new ImageLocation( info[0].UUID, 
+                                  info[0].SUBVOLUMEID,
+                                  info[1],
+                                  storetype,
+                                  main,
+                                  userid);
             
     }
 
@@ -60,29 +88,48 @@ class ImageLocation
 
         var res = await getrows(db,sql,[]);
         
+        var ret = [];
+
         if (res.success)
         {
             for (var i in res.rows)
             {
-                
+                var row = res.rows[i];
+                ret.append(
+                    new ImageLocation(  row.MounPointUUID, 
+                                        row.SubVolumeId,
+                                        row.SubVolumePath,
+                                        row.Directory,
+                                        EStoreType.ToStoreType(row),
+                                        row.Main,
+                                        row.UserId
+                                        ));
             }
         }
+        else
+        {
+            throw "Sql error "+res.err;
+        }
+
+        return ret;
     }
 
     /**
      * Best not to call this directly, instead use GetAll() or GetActive() or CreateNew()
      * @param {*} uuid 
      * @param {*} subvolumeid 
+     * @param {*} subvolumepath
      * @param {*} path 
      * @param {*} storetype 
      * @param {*} main 
      * @param {*} userid 
      */
-    constructor(uuid, subvolumeid, path, storetype,  main=false, userid=null)
+    constructor(uuid, subvolumeid, subvolumepath, path, storetype,  main=false, userid=null)
     {
         
         this.UUID = uuid;
         this.SubVolumeId = subvolumeid;
+        this.SubVolumePath = subvolumepath;
         this.Path = path;
         // initialize mountinfo.
         this.Active = true;
